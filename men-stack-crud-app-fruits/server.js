1f66189008829a82259a332487c3e88ff0580a12
noa-filosof1 <filosofnoa@gmail.com>
2026-01-08 15:39:02 +0000

    crud up eod thursday

--- men-stack-crud-app-fruits/server.js
+++ men-stack-crud-app-fruits/server.js
@@ -10,6 +10,7 @@ const Fruit = require('./models/fruit.js');
 
 // use express (using app)
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
 mongoose.connect(process.env.MONGODB_URI); // Connects to mongoDB using the connection string (our key) 
@@ -23,9 +24,28 @@ app.get('/', async (req, res) => {
   res.render('index.ejs');
 })
 
+// Get fruits (index page)
+app.get('/fruits', async (req, res) => {
+  const allFruits = await Fruit.find(); // find all the fruits in my fruits collection from MongoDB
+  console.log('all of my fruits pls', allFruits); // log out the fruits
+  res.render('fruits/index.ejs', { fruits: allFruits });
+})
+
 // get for new fruits
 app.get("/fruits/new", (req, res) => {
   res.render('fruits/new.ejs');
+})
+
+// post fruits 
+app.post('/fruits', async (req, res) => {
+  if (req.body.isReadyToEat === 'on') {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  console.log(req.body); // we get the req body (the request body) from the form data
+  await Fruit.create(req.body); // create fruit in MongoDB
+  res.redirect('/fruits'); // once we get that data, redirect back to the form creation page 
 })
 
 // listen 
