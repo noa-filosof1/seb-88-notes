32d5cec086b72bc40d5996b20b8fc6221f1c2704
noa-filosof1 <filosofnoa@gmail.com>
2026-01-09 13:57:13 +0000

    fruits app complete

--- men-stack-crud-app-fruits/server.js
+++ men-stack-crud-app-fruits/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 // import with ecmascript 
 // import express from 'express';
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config(); // loads the environment variables from the .env file 
@@ -11,6 +12,7 @@ const Fruit = require('./models/fruit.js');
 // use express (using app)
 const app = express();
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); 
 app.use(morgan('dev'));
 
 mongoose.connect(process.env.MONGODB_URI); // Connects to mongoDB using the connection string (our key) 
@@ -20,6 +22,7 @@ mongoose.connection.on('connected', () => {
 
 
 // routes 
+// root route
 app.get('/', async (req, res) => {
   res.render('index.ejs');
 })
@@ -36,6 +39,32 @@ app.get("/fruits/new", (req, res) => {
   res.render('fruits/new.ejs');
 })
 
+// show route - specific data 
+// url looks something like fruits/:id
+app.get('/fruits/:fruitId', async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  console.log('found fruit', foundFruit);
+  res.render("fruits/show.ejs", { fruit: foundFruit });
+})
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect('/fruits');
+})
+
+// update route 
+app.put("/fruits/:fruitId", async (req, res) => {
+  if (req.body.isReadyToEat === 'on') {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  // first parameter is the req.params.fruitId (which is the id we use to find the fruit)
+  // the next parameter is the req.body which is the data from the form to update the fruit
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  res.redirect(`/fruits/${req.params.fruitId}`);
+})
+
 // post fruits 
 app.post('/fruits', async (req, res) => {
   if (req.body.isReadyToEat === 'on') {
@@ -48,7 +77,23 @@ app.post('/fruits', async (req, res) => {
   res.redirect('/fruits'); // once we get that data, redirect back to the form creation page 
 })
 
+// edit route (can put at the bottom because it does not conflict)
+app.get('/fruits/:fruitId/edit', async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  console.log('found fruit for edit', foundFruit);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  })
+}) 
+
 // listen 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 })
+
+
+// edit route 
+// GET /fruits/:fruitId/edit
+// have a look at routing table for the edit route + create route 
+// update route
+// /fruits/:fruitId
